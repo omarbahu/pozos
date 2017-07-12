1fcd3b3aba8874ea80ff6ec05613a65774dff027
omarbahu <omarbahu@gmail.com>
2017-07-12 04:14:37 +0000

    ok ∑

--- operaciones/include.php
+++ operaciones/include.php
@@ -11,7 +11,7 @@ class DBManager{
                 $this->BaseDatos = "pozos";
                 $this->Servidor = "127.0.0.1";
                 $this->Usuario = "omarbahu";
-                $this->Clave = "";
+                $this->Clave = ""; 
                 //$this->Usuario = "root";
                 //$this->Clave = "";
         }
